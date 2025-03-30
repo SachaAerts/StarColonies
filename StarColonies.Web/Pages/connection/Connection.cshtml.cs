d23319a68e2e506df696cd2784825ac04bebfc04
Sacha Aerts <sachaaerts@Mac-mini-de-Sacha.local>
2025-03-31 00:27:59 +0200

    interface connection and register page done

--- StarColonies.Web/Pages/connection/Connection.cshtml.cs
+++ StarColonies.Web/Pages/connection/Connection.cshtml.cs
@@ -1,11 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StarColonies.Web.wwwroot.models;
 
 namespace StarColonies.Web.Pages.connection;
 
 public class Connection : PageModel
 {
+    [BindProperty] public ConnectionRegisterModel ConnectionRegister { get; set; } = new();
+    
     public void OnGet()
     {
+    }
+    
+    public IActionResult OnPostLogin()
+    {
+        Console.WriteLine($"[DEBUG] Username: {ConnectionRegister.EmailOrUsernameConnection}, Password: {ConnectionRegister.PasswordConnection}");
         
+        return Page();
+    }
+
+    public IActionResult OnPostRegister()
+    {
+        Console.WriteLine($"[DEBUG] Email: {ConnectionRegister.EmailRegister}, Password: {ConnectionRegister.PasswordRegister}, PasswordConfirmation: {ConnectionRegister.ConfirmPasswordRegister}");
+        
+        return Page();
     }
 }
---
+++ StarColonies.Web/wwwroot/models/ConnectionRegisterModel.cs
@@ -0,0 +1,14 @@
+namespace StarColonies.Web.wwwroot.models;
+
+public class ConnectionRegisterModel
+{
+    public string EmailOrUsernameConnection { get; set; } = "";
+    
+    public string PasswordConnection { get; set; } = "";
+    
+    public string EmailRegister { get; set; } = "";
+    
+    public string PasswordRegister { get; set; } = "";
+    
+    public string ConfirmPasswordRegister { get; set; } = "";
+}
