82f0cc8bdffe64fba4e196a93a989de5f7219c07
Sirius <boukhanoucheayoubpro@gmail.com>
2025-04-15 21:19:00 +0200

    Update

--- StarColonies.Web/Pages/Map.cshtml.cs
+++ StarColonies.Web/Pages/Map.cshtml.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StarColonies.Infrastructures.Data;
+using StarColonies.Infrastructures.Data.Entities.Missions;
 
 namespace StarColonies.Web.Pages;
 
-public class Map : PageModel
+public class Map(StarColoniesDbContext context) : PageModel
 {
-    public void OnGet()
+    
+    public List<PlanetEntity> Planets { get; set; } = new();
+    
+    public async void OnGet()
     {
-        
+        Planets = await context.Planets
+            .Include(p => p.Missions)
+            .ToListAsync();
     }
 }
