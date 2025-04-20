d693b14e2cdd1aacfdc1f01fc6b37d874c2151c0
sachaaerts <sa.aerts@student.helmo.be>
2025-04-20 17:00:43 +0200

    index done

--- StarColonies.Web/Pages/Index.cshtml.cs
+++ StarColonies.Web/Pages/Index.cshtml.cs
@@ -1,13 +1,22 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StarColonies.Domains.Models.Colony;
+using StarColonies.Domains.Repositories;
 
 namespace StarColonies.Web.Pages;
 
-public class IndexModel(ILogger<IndexModel> logger) : PageModel
+public class IndexModel(ILogger<IndexModel> logger, IColonyRepository colonyRepository)
+    : PageModel
 {
     private readonly ILogger<IndexModel> _logger = logger;
 
-    public void OnGet()
+    public IList<ColonyModel> TopColonies { get; set; } = [];
+
+    public async Task OnGetAsync()
     {
+        TopColonies = await colonyRepository.GetTop10ColoniesAsync();
+        foreach (var colony in TopColonies)
+        {
+            Console.WriteLine("Image: " + colony.LogoPath + "\n");
+        }
     }
 }
