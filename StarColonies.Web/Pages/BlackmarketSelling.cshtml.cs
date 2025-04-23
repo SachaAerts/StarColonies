89dd9e2213b7d04965f47a9355373aef8465ec2b
Q230119 <a.ciesiolka@student.helmo.be>
2025-04-23 05:49:02 +0200

    HTML du Blackmarket

---
+++ StarColonies.Web/Pages/BlackmarketSelling.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StarColonies.Web.Pages;
+
+public class BlackmarketSelling : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
