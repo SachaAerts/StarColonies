18838f14fa76961b9ba71eb9a380af5a21d22ee1
Sirius <boukhanoucheayoubpro@gmail.com>
2025-04-27 22:17:03 +0200

    update

---
+++ StarColonies.Web/wwwroot/js/notifyConnection.js
@@ -0,0 +1,26 @@
+﻿import { NotificationContext } from './components/notifications/NotificationContext.js';
+import { NotyfStrategy } from './components/notifications/NotyStrategy.js';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const notificationContext = new NotificationContext(new NotyfStrategy());
+
+
+    const validationErrors = document.querySelectorAll(".text-danger");
+    validationErrors.forEach(errorElement => {
+        const errorMessage = errorElement.textContent.trim();
+        if (errorMessage.length > 0) {
+            notificationContext.error(errorMessage);
+        }
+    });
+
+    const validationSummary = document.querySelector("[asp-validation-summary='ModelOnly']");
+    if (validationSummary) {
+        const summaryMessages = validationSummary.querySelectorAll("li");
+        summaryMessages.forEach(li => {
+            const message = li.textContent.trim();
+            if (message.length > 0) {
+                notificationContext.error(message);
+            }
+        });
+    }
+});
